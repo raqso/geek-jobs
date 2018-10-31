7262f8cc7200428fae960b64523f71203927dc99
Adam Producent <raqsasg@gmail.com>
2018-10-31 23:17:03 +0100

    Initial impelementation of Autocomplete components

---
+++ src/front/src/components/AutoComplete.tsx
@@ -0,0 +1,78 @@
+import * as React from 'react';
+
+const positionSuggestionsEndpoint =
+'http://localhost:3000/suggest?position=';
+// const locationSuggestionsEndpoint = 'http://localhost:3000/suggest?location=';
+
+interface AutoCompleteProps {
+  placeholder: string;
+  maxSuggestions: number;
+}
+
+interface AutoCompletePropsState {
+  value: string;
+  isFocus: boolean;
+  suggestions: string[];
+}
+
+export default class AutoComplete extends React.Component<
+  AutoCompleteProps,
+  AutoCompletePropsState
+> {
+
+  constructor(props: AutoCompleteProps) {
+    super(props);
+    this.state = {
+      isFocus: false,
+      suggestions: [],
+      value: ''
+    };
+  }
+
+  public render() {
+    return (
+      <div className="input-field first-wrap" onFocus={() => this.setState({ isFocus: true })} onBlur={() => {
+        this.setState({ isFocus: false });
+        console.log('div out!');
+    }}>
+        <input
+          id="position"
+          type="text"
+          placeholder={this.props.placeholder}
+          onChange={event => this.handleChange(event)}
+          
+          value={this.state.value}
+          
+        />
+        {this.renderSuggestions()}
+      </div>
+    );
+  }
+
+  private handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ value: event.target.value });
+    this.getSuggestions();
+  }
+
+  private async getSuggestions() {
+    await fetch(positionSuggestionsEndpoint + this.state.value)
+      .then(response => response.json())
+      .then(suggestions =>
+        this.setState({
+          suggestions: suggestions.slice(0, this.props.maxSuggestions)
+        })
+      );
+  }
+
+  private renderSuggestions() {
+      return (
+        <ul hidden={!this.state.isFocus}>
+          {this.state.suggestions.map(suggestion => (
+            <li key={suggestion} value={suggestion} onClick={() => this.setState({value: suggestion})} >
+              {suggestion}
+            </li>
+          ))}
+        </ul>
+      );
+  }
+}
--- src/front/src/components/SearchBox.tsx
+++ src/front/src/components/SearchBox.tsx
@@ -1,40 +1,54 @@
 import * as React from 'react';
-import './SearchBox.css'; 
+import AutoComplete from './AutoComplete';
+import './SearchBox.css';
 
 interface SearchBoxProps {
-    positionPlaceholder: string; 
-    locationPlaceholder: string;
-    searchButtonText: string;
-    onClick: ()=>void;
+  positionPlaceholder: string;
+  locationPlaceholder: string;
+  searchButtonText: string;
+  onClick: () => void;
 }
 
 interface SearchBoxState {
-    isExpanded: boolean;
+  positionValue: string;
+  locationValue: string;
 }
-export default class SearchBox extends React.Component<SearchBoxProps, SearchBoxState> {
-    constructor(props: SearchBoxProps) {
-        super(props);
-        this.state = {isExpanded: false};
-    }
+export default class SearchBox extends React.Component<
+  SearchBoxProps,
+  SearchBoxState
+> {
+  constructor(props: SearchBoxProps) {
+    super(props);
+    this.state = { locationValue: '', positionValue: '' };
+  }
 
-    public render() {
-        return <div className="s01">
+  public render() {
+    return (
+      <div className="s01">
         <form>
           <fieldset>
             <legend>Znajdź pracę w IT</legend>
           </fieldset>
           <div className="inner-form">
-            <div className="input-field first-wrap">
-              <input id="search" type="text" placeholder={this.props.positionPlaceholder} />
-            </div>
-            <div className="input-field second-wrap">
-              <input id="location" type="text" placeholder={this.props.positionPlaceholder} />
-            </div>
+            <AutoComplete placeholder={this.props.positionPlaceholder} maxSuggestions={10} />
+            <AutoComplete placeholder={this.props.locationPlaceholder} maxSuggestions={5} />
             <div className="input-field third-wrap">
-              <button className="btn-search" type="button" onClick={this.props.onClick}>{this.props.searchButtonText}</button>
+              <button
+                className="btn-search"
+                type="button"
+                onClick={this.props.onClick}
+              >
+                {this.props.searchButtonText}
+              </button>
             </div>
           </div>
         </form>
       </div>
-    }
+    );
+  }
+
+  /* private handleLocationChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ locationValue: event.target.value });
+    console.log(event.target.value);
+  } */
 }
