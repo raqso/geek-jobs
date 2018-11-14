d669bb93399e9b1b0cff3955f40975e8f66b876d
Adam Producent <raqsasg@gmail.com>
2018-11-14 23:05:19 +0100

    node-cron testing

--- src/back/api/server.ts
+++ src/back/api/server.ts
@@ -1,6 +1,12 @@
 import app from './app';
+import cron from 'node-cron';
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log('Express server listening on port ' + PORT);
+});
+
+cron.schedule('45 * * * *', () => {
+  console.log('running a task every 45 minutes');
 });
