5e000919298f80de025def653ec6220e8991dc83
Adam Producent <raqsasg@gmail.com>
2018-12-11 21:12:30 +0100

    Fixed cron interval

--- src/back/Cron.ts
+++ src/back/Cron.ts
@@ -1,7 +1,7 @@
 import cron from 'node-cron';
 import launchScrapping from './jobs-scrapper/LaunchScrapping';
 
-cron.schedule('* /6 * * *', () => {
+cron.schedule('* */6 * * *', () => {
   console.log('running scrapping every 6 hours');
   try {
     launchScrapping();
