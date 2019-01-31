77fba667125502ac09f70cf88590f54cb1722290
Adam Producent <raqsasg@gmail.com>
2019-01-31 19:30:19 +0100

    removed unused import

--- src/front/components/OffersCounter.tsx
+++ src/front/components/OffersCounter.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-/* import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar'; */
 
 const OffersCounter = (props: {
   offersLength: number;
