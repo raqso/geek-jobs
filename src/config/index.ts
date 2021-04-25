7bfd82770eda7c19f0706e3bf3879fac6c5e404c
Adam <raqsasg@gmail.com>
2021-04-25 19:37:21 +0200

    Change API env var to API_URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const config = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost/jobs',
-  api: process.env.API || '/api',
+  api: process.env.API_URL || '/api',
   port: parseInt(`${process.env.PORT || 5000}`),
 };
 
