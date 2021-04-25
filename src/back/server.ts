78a30b85e701570de79948971ac374ff109b096a
Adam <raqsasg@gmail.com>
2021-04-25 19:09:51 +0200

    Adjust port env variable

--- src/back/server.ts
+++ src/back/server.ts
@@ -1,7 +1,8 @@
 import app from './api/Api';
 import express = require('express');
+import config from '../config';
 
-const PORT = parseInt(`${process.env.PORT}`) || 5000;
+const PORT = config.port;
 
 app.use(express.static('./dist/front'));
 app.listen(PORT, '0.0.0.0', () => {
