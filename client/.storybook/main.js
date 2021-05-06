c9566dd0998928c38f27b7c8fac6edc63c63e059
Adam <raqsasg@gmail.com>
2021-05-06 11:39:54 +0200

    Fix storybook compilation

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -10,8 +10,7 @@ module.exports = {
       name: "@storybook/preset-scss",
       options: {
         cssLoaderOptions: {
-           modules: true,
-           localIdentName: '[name]__[local]--[hash:base64:5]',
+           modules: true
         }
       }
     }
