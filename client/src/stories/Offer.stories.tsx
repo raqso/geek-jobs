56403592385651e235cbe5487bee3e9e2b136617
blady <belazdzislaw@gmail.com>
2021-05-07 19:09:03 +0200

    Add MinimalOffer story

--- client/src/stories/Offer.stories.tsx
+++ client/src/stories/Offer.stories.tsx
@@ -13,13 +13,13 @@ export const NewOffer = Template.bind({});
 NewOffer.args = {
   companyLogo:
     "https://cdn.dribbble.com/users/31864/screenshots/3666062/free_logos_dribbble_ph.jpg?compress=1&resize=400x300",
+  link: "https://geek-jobs.vercel.app/",
   position: "Software Tester",
   company: "Geek-Jobs Poland",
-  salary: { from: 3000, to: 7000, currency: "PLN" },
-  location: "Wroclaw, PL",
-  link: "https://geek-jobs.vercel.app/",
   tags: ["English", "CI/CD", "JIRA", "Azure DevOps"],
+  salary: { from: 3000, to: 7000, currency: "PLN" },
   date: "2 days ago",
+  location: "Wroclaw, PL",
   isNew: true,
 };
 
@@ -27,11 +27,18 @@ export const OldOffer = Template.bind({});
 OldOffer.args = {
   companyLogo:
     "https://cdn.dribbble.com/users/31864/screenshots/3666062/free_logos_dribbble_ph.jpg?compress=1&resize=400x300",
+  link: "https://geek-jobs.vercel.app/",
   position: "JavaScript Developer",
   company: "Geek-Jobs Poland",
-  salary: { from: 5000, to: 10000, currency: "PLN" },
-  location: "Wroclaw, PL",
-  link: "https://geek-jobs.vercel.app/",
   tags: ["WEB DEVELOPMENT", "REACT", "TYPESCRIPT"],
   date: "2 days ago",
+  salary: { from: 5000, to: 10000, currency: "PLN" },
+  location: "Wroclaw, PL",
+};
+
+export const MinimalOffer = Template.bind({});
+MinimalOffer.args = {
+  position: "Scrum Master",
+  link: "https://geek-jobs.vercel.app/",
+  company: "Geek-Jobs Poland",
 };
