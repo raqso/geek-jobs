06281371990c85f9d48858c280a2c13e6fd632ff
blady <belazdzislaw@gmail.com>
2021-05-08 13:04:14 +0200

    Add newIcon.svg

--- client/src/components/Offer/NewTag/NewTag.tsx
+++ client/src/components/Offer/NewTag/NewTag.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styles from "./NewTag.module.scss";
+import newIcon from "../assets/newIcon.svg";
 
 export const NewTag = () => {
-  return <div className={styles.newTag}>NEW</div>;
+  return (
+    <div className={styles.newIcon}>
+      <img src={newIcon} alt={newIcon} />
+    </div>
+  );
 };
