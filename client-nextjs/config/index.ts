a9c911dc7a0f4f4faf3ab01842017b30cad562a0
blady <belazdzislaw@gmail.com>
2021-05-29 19:18:03 +0200

    Remove temporary client-nextjs folder

--- client-nextjs/config/index.ts
+++
@@ -1,5 +0,0 @@
-const config = {
-  api: process.env.NEXT_PUBLIC_API_URL
-};
-
-export default config;
--- client-nextjs/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import "../styles/globals.css";
-import "../components/App.css";
-import "../components/SearchBox.css";
-import type { AppProps } from "next/app";
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-export default MyApp;
